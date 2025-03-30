84a0c13c1b513d61dbef8d17cb3f2715cbbbf3f8
Gg <trashboatd.gg.sam@gmail.com>
2025-03-30 23:10:38 +0200

    Adds fundings

---
+++ src/data/fundings.ts
@@ -0,0 +1,87 @@
+export const FUNDINGS = {
+  cities: {
+    sulzbach: {
+      "Private Trinkwasserenthärtungsanlagen": {
+        policy:
+          "für private Trinkwasserenthärtungsanlagen (Entkalkungsanlagen) (Stand 03.11.2021)",
+        description:
+          "Anlagen, deren Wirkungsweise nach DVGW-Kriterien geprüft sind oder gleichwertig wirkungsvoll arbeiten, geltenden Vorschriften, technischen Regeln und Anforderungen nach aktuellem Stand der Technik entsprechen und an das Wasserversorgungsnetz angeschlossen sind.",
+        funding:
+          "30 % des Kaufpreises, maximal jedoch 500 €, 1 Anlage pro Hausanschluss",
+        link: "https://cms.sulzbach-taunus.de/media/downloads/RL-Trinkwasserenthaertungsanlagen-Stand-03_11_2021zuletzt-geaendert-am-05-07-2023.pdf",
+      },
+      Entsiegelungsmaßnahmen: {
+        policy:
+          "zum Rückbau von versiegelten (überbauten und wasserundurchlässig befestigten) Flächen (Entsiegelungsmaßnahmen) (Stand 03.11.2021)",
+        description:
+          "Für eine Förderung gelten als zuwendungsfähige Kosten die durch Rechnung nachzuweisenden Aufwendungen für Material inkl. Einbaukosten. Planungskosten sind nicht förderfähig. Eine Förderung ist nur bei freiwilligen Maßnahmen möglich.",
+        funding:
+          "50 % der Kosten, maximal jedoch 12,50 € / qm entsiegelter Fläche",
+        link: "https://cms.sulzbach-taunus.de/media/downloads/RL-Entsiegelung-Stand-03_11_2021-zuletzt-bearbeitet-am-05-07-2023.pdf",
+      },
+      "Maßnahmen zur Herstellung von Dach- und Fassadenbegrünung": {
+        policy:
+          "für die Geährung von Zuschüssen zur Herstellung von Dach- und Fassadenbegrünung (Stand 03.11.2021)",
+        description:
+          "Für eine Förderung von Dach-und / oder Fassadenbegrünung (kombinierte Förderung möglich) gelten als zuwendungsfähige Kosten die durch Rechnung nachzuweisenden Aufwendungen für Material inkl. Einbaukosten. Planungskosten sind nicht förderfähig. Eine Förderung ist nur bei freiwilligen Maßnahmen möglich.",
+        funding:
+          "Dachbegrünung: 50 % der Kosten, maximal jedoch 15 € / qm extensiver Dachbegrünung, sowie 25 € / qm bei intensiver Dachbegrünung. Fassadenbegrünung: 50 % der Kosten, maximal jedoch 15 € / qm extensiver Fassadenbegrünung.",
+        link: "https://cms.sulzbach-taunus.de/media/downloads/RL-Dach-und-Fassadenbegruendung-Stand-03_11_2021-zuletzt-bearbeitet-am-05-07-2023.pdf",
+      },
+      "E-Lastenräder": {
+        policy: "beim Neukauf von E-Lastenrädern (Stand 03.11.2021)",
+        description:
+          "Die Förderung ist auf ein E-Lastenmotor je Haushalt / Gewerbebetrieb beschränkt.",
+        funding: "30 % des Kaufpreises, maximal jedoch 500 €",
+        link: "https://cms.sulzbach-taunus.de/media/downloads/RL-E-Lastenraeder-Stand-03_11_2021-zuletzt-bearbeitet-am-05-07-2023.pdf",
+      },
+      "Höchst-effiziente Haushaltsgeräte": {
+        policy:
+          "beim Neukauf von höchst-effizienten Haushaltsgeräten (Stand 03.11.2021) ",
+        description:
+          'Großgeräte Elektroherd, Kühlschrank, Gefrierschrank, Gefriertruhe, Geschirrspülmaschine, Waschmaschine sowie Wäschetrockner (sog. Große Weißware ") mit der Energieeffizienzklasse A (gem. dem am 01.03.2021 neu in Kraft getretenen neuen Energielabel, ehemals Energieeffizienzklasse A+++)',
+        funding:
+          "15 % des Kaufpreises (ohne Lieferung und Installation). maximal jedoch 150 €. Die Förderung ist beschränkt auf insgesamt 1 Gerät pro Jahr und Haushalt.",
+        link: "https://cms.sulzbach-taunus.de/media/downloads/RL-Haushaltsgeraete-Stand-03_11_2021-zuletzt-bearbeitet-am-05.07.2023.pdf",
+      },
+      "Steckbare Mini-PV-Anlagen / Balkonmodule": {
+        policy:
+          "zu steckbaren Mini-PV Anlagen / Balkonmodule (Stand 16.07.2024)",
+        description:
+          "Gefördert werden steckbare Mini-PV Anlagen / Balkonmodule, die alle anzuwendenden Normen für fest installierte Stromerzeugungsgeräte / VDE-Normen erfüllen. Für den Anschluss der Anlage ist ein Wieland-Stecker zu verwenden.",
+        funding:
+          "100 € / Modul, maximal jedoch 50 % des Kaufpreises. Die 2 Module pro Wohneinheit.",
+        link: "https://cms.sulzbach-taunus.de/media/downloads/RL-Balkonmodule-Stand-16-07-2024.pdf",
+      },
+      "Photovoltaik-anlagen": {
+        policy: "für Photovoltaik-anlagen (Stand 16.07.2024)",
+        description: "",
+        funding:
+          "100 € / kWp, max. 1.000 € / Anlage. Die Bezuschussung von Photovoltaikanlagen ist auf eine Förderung je Grundstück beschränkt.",
+        link: "https://cms.sulzbach-taunus.de/media/downloads/RL-Photovoltaikanlagen-Stand-16-07-2024.pdf",
+      },
+      "Stromspeicher Solaranlagen": {
+        policy: "zu Stromspeichern (Stand 16.07.2024)",
+        description: "",
+        funding: "125 € / kWh Speicherkapazität. Max. 1250 € / Grundstück",
+        link: "https://cms.sulzbach-taunus.de/media/downloads/RL-Stromspeicher-Stand-16-07-2024.pdf",
+      },
+      "Solaranlagen zur Warmwasseraufbereitung und zur Heizungsunterstützung": {
+        policy:
+          "Zuschüsse zum Bau von Solaranlagen zur Warmwasseraufbereitung und zur Heizungsunterstützung.",
+        description:
+          "Solarthermie bedeutet Sonnenenergie zur Wärmegewinnung (Warmwasser- und / oder Heizungsunterstützung) zu nutzen. Haushalte mit einem erhöhten Warmwasserbedarf können besonders von solarthermischen Anlagen profitieren und spürbar Energiekosten sparen.",
+        funding:
+          "50 € / qm Flachkollektorfläche. 80 € / qm Röhrenkollektor. Max. jedoch mit 300,00 € für Warmwasseraufbereitung je Objekt / Gebäude und max. 500 € für Heizungsunterstützung je Objekt / Gebäude.",
+        link: "https://cms.sulzbach-taunus.de/media/downloads/RL-Solaranlagen-WarmwasserHeizung-Stand-30_05_2017-zuletzt-bearbeitet-am-05-07-2023.pdf",
+      },
+    },
+  },
+};
+
+export interface FundingEntry {
+  policy: string;
+  description: string;
+  funding: string;
+  link?: string;
+}
